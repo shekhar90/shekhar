8b0404497bbdd01b7c1642fd2341c08261e60ed5
Shekhar <shekharsuman@triconinfotech.com>
2018-02-12 20:04:57 +0530

    material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { MaterialuiModule } from './materialui/materialui.module'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialuiModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
---
+++ src/app/materialui/materialui.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatTabsModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule
+  ],
+  declarations: [],
+  exports: [
+    MatTabsModule
+  ]
+})
+export class MaterialuiModule { }
