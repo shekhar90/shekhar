ede8444ed8e96a0034923d54e876bf7dbd829a39
Shekhar <shekharsuman@triconinfotech.com>
2018-03-13 20:46:28 +0530

    signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
 
 @Component({
   selector: 'app-signup',
@@ -7,13 +9,24 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
   @Output() onSigninClick = new EventEmitter<boolean>();
-  constructor() { }
-  model: any;
+  constructor(private httpClient: HttpClient) { }
+  model = new User();
   ngOnInit() {
   }
   handleSigninClick() { // only for routing to signin page
     this.onSigninClick.emit();
   }
   handleSignupClick() { // post form data to backend
+    this.httpClient.post('http://localhost:3000/signup', this.model)
+    .subscribe(data => {
+      const res: any = data;
+      if (res.status === 'error') { // Error in adding new user
+        console.log(res.data);
+      } else if (res.status === 'success' && res.data.msg === 'user already exists') { // user with this email already registered
+        console.log(res.data);
+      } else { // user added goto login
+        this.handleSigninClick();
+      }
+    });
   }
 }
