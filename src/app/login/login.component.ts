bb92033a3c481ab2637a7a94c1bbc64ea81a2eae
Shekhar <shekharsuman@triconinfotech.com>
2018-03-16 19:52:55 +0530

    login signup update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { UserLogin } from '../user-login';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,26 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   @Output() onSignupClick = new EventEmitter<boolean>();
-  constructor() { }
-
+  constructor(private httpClient: HttpClient, private router: Router) { }
+  model = new UserLogin();
   ngOnInit() {
   }
   handleSignupClick() {
     this.onSignupClick.emit();
   }
-
+  gotoPractice() {
+    this.router.navigate(['/practice']);
+  }
+  handleLoginClick() { // post form data to backend
+    this.httpClient.post('http://localhost:3000/login', this.model)
+    .subscribe(data => {
+      const res: any = data;
+      if (res.status === 'error') { // Error in adding new user
+        console.log(res.data);
+      } else { // valid user added goto homepage
+        console.log('go to home page');
+        this.gotoPractice();
+      }
+    });
+  }
 }
