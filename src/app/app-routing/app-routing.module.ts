6030c337585daebe4a3f95f54216e0590b2c8242
SHEKHAR\shekharsuman <shekharsumannit90@gmail.com>
2018-05-01 17:31:36 +0530

    AuthGuard for protecting specific routes from unauthorised users

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { PracticeComponent } from '../practice/practice.component';
 import { LoginSignupComponent } from '../login-signup/login-signup.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'practice', component: PracticeComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'practice', component: PracticeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginSignupComponent, data: { login: true } },
   { path: 'signup', component: LoginSignupComponent, data: { login: false } }
 ];
